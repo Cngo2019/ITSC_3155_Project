5dd44848ba6fa238fe46952a058d3e3467524f0a
Cngo2019 <canngo215@gmail.com>
2022-03-15 08:56:17 -0400

    created the requirements.txt

--- UserPost.py
+++ UserPost.py
@@ -15,10 +15,12 @@ class UserPost:
     def attach_reply(self, user_reply):
         self.replies.append(user_reply)
 # TESTING:
+# User creates a post on our website
 user_1 = UserPost("I_need_homework_help120", "HOW DO I CODE THIS PROJECT?", "Hey guys, I have no idea what I am doing. I really don't. Please help.", "Math")
+# Another user sees the post and then creates a reply. In the back-end it takes user 1's post and attaches it as a reply.
 user_2 = UserReply("Stop_cheating_guy_123", "Bro just buy a chegg subscription", user_1)
 user_1.attach_reply(user_2)
-
+# Same concept applied to the 2nd responder
 user_3 = UserReply("Toxic_Poster", "You suck.", user_1)
 user_1.attach_reply(user_3)
 
