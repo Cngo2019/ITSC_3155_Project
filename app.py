c78f94f6f25b070ce664386b5504187d64fcd2f0
Cngo2019 <canngo215@gmail.com>
2022-03-26 10:53:59 -0400

    obtaining post information implemented

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+# A temporary array to store our form information.
+A = []
+# Go to the create page and create the HTML page
+@app.get('/create')
+def create():
+    return render_template('create.html')
+
+# After submitting the information to the obtain_post_info route, we can obtain
+# all the neccessary info
+@app.post('/obtain_post_info')
+def add_post():
+    # Obtain the neccessary information sent from the form
+    post_title = request.form.get('post_type')
+    post_body = request.form.get('post_body')
+    post_subject = request.form.get('post_subject')
+    tuple_data = (post_title, post_body, post_subject)
+    # Just add it to the temporary structure
+    A.append(tuple_data)
+    return redirect('/view_all')
+# When going to the view_all page just simply render it.
+@app.get('/view_all')
+def all_posts():
+    return render_template('view_all.html', A = A)
