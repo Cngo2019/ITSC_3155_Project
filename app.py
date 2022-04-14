6a1ce77f7707a2d4658c70748da2aa04b86b9968
Cngo2019 <canngo215@gmail.com>
2022-04-14 09:44:23 -0400

    imported package

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
