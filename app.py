67c7b5cb7d1e53fb42dffc04ee1781038c7af71e
reygalaviz <reygalaviz26@yahoo.com>
2022-05-06 15:09:25 -0400

    updating db env var to heroku's db env var

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('CLEARDB_DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = os.getenv('SECRET_KEY')
 
